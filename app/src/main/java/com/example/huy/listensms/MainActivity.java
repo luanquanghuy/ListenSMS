f695c147982628485aabfa3ac707927911741e46
Quang Huy <luanquanghuy@gmail.com>
2018-06-24 17:39:29 +0700

    Update new 2

--- app/src/main/java/com/example/huy/listensms/MainActivity.java
+++ app/src/main/java/com/example/huy/listensms/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.huy.listensms;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int  REQUEST_CODE_SMS = 1991;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS, Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS}, REQUEST_CODE_SMS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == REQUEST_CODE_SMS && grantResults.length >= 3 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&  grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Đăng ký thất bại", Toast.LENGTH_SHORT).show();
+        }
     }
 }
